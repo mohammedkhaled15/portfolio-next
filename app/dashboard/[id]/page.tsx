7a4124cc73d2157df109b9722747e8064d167a4f
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-08-16 19:34:06 +0300

    .

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,16 +1,60 @@
 "use client"
 import { useParams } from "next/navigation"
-import { useSearchParams } from "next/navigation"
+import { projects } from "@app/portfolio/projects"
+import React, { useState } from "react"
+
+interface ProjectData {
+  id: number;
+  name: string;
+  skills: string[];
+  demoLink: string;
+  repoLink: string;
+  imgUrl: string;
+}
 
 const EditPage = () => {
-  const params = useParams()
-  const searchParams = useSearchParams()
-  // console.log(searchParams)
+  const searchParams = useParams()
+  const projectData = projects.filter(project => searchParams.id == project.id.toString())[0]
+
+
+
+  const [editedProjectData, setEditedProjectData] = useState<ProjectData>(projectData)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.name)
+    setEditedProjectData(prev => ({ ...prev, [e.currentTarget?.name]: e.currentTarget?.value } as ProjectData))
+  }
+
+
+  console.log(editedProjectData)
   return (
-    <div>
-      {JSON.stringify(params)}
-      {JSON.stringify(searchParams)}
+    <div className="flex flex-col gap-3 items-center">
+      <h3 className="self-center my-10 ">Create New Project</h3>
+      <form className="w-[90%] max-w-[700px] ">
+        <div className="relative z-0 w-full mb-6 group">
+          <input value={editedProjectData.name} type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required onChange={handleChange} />
+          <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Project Name</label>
+        </div>
+        {/* <div className="relative z-0 w-full mb-6 group">
+          <input value={editedProject.skills.map((skill: string) => `${skill} - `)} type="text" name="skills" id="skills" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <label htmlFor="skills" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Skills (html, nodeJs)</label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <input type="text" name="demoLink" id="demoLink" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <label htmlFor="demoLink" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Demo Link</label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <input type="text" name="repoLink" id="repoLink" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <label htmlFor="repoLink" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Repo Link</label>
+        </div>
+        <div className="relative z-0 w-full mb-6 group">
+          <input type="text" name="imgUrl" id="imgUrl" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+          <label htmlFor="imgUrl" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Image Url</label>
+        </div> */}
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
+      </form>
     </div>
+    // <h1>hello</h1>
   )
 }
 
